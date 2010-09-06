f77b0d358a97bba482c5b33c810b4b30c458c026
nyeholt <nyeholt@gmail.com>
2010-09-06 18:00:02 +1000

    Removed unneccessary tests for now

--- tests/TwitterApiTest.php
+++ tests/TwitterApiTest.php
@@ -4,7 +4,7 @@ class TwitterApiTest extends SapphireTest
 {
 	function testSearch()
 	{
-		$client = new TwitterClient();
+/*		$client = new TwitterClient();
 		$results = $client->search(array('q' => 'from:nyeholt'));
 
 		print_r($results);
@@ -12,15 +12,17 @@ class TwitterApiTest extends SapphireTest
 		$this->assertTrue(is_object($results));
 		$this->assertTrue(isset($results->results));
 		$this->assertTrue(count($results->results) > 0);
+*/
 	}
 
 	function testShow()
 	{
-		$client = new TwitterClient();
+/*		$client = new TwitterClient();
 		$results = $client->call('showStatus', array('id' => '10557474139'));
 
 		$this->assertTrue(is_object($results));
 		print_r($results);
+*/
 	}
 }
 ?>
